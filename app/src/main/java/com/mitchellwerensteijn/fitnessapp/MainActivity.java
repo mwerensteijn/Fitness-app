4b98eca30d44683dbb2c8d3b2ba4a905289e1138
Mitchell Werensteijn <m.werensteijn@innoseis.com>
2016-04-03 20:21:54 +0200

    New layout

--- app/src/main/java/com/mitchellwerensteijn/fitnessapp/MainActivity.java
+++ app/src/main/java/com/mitchellwerensteijn/fitnessapp/MainActivity.java
@@ -1,19 +1,28 @@
 package com.mitchellwerensteijn.fitnessapp;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -37,50 +46,70 @@ import com.github.mikephil.charting.data.LineDataSet;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] drawerMenuItems = new String[]{"Workout", "History", "Settings"};
-    private DrawerLayout drawerLayout;
-    private ListView drawerList;
-    private CharSequence toolbarTitle;
-    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private Toolbar toolbar;
+    private android.support.design.widget.TabLayout mTabs;
+    private ViewPager mViewPager;
 
+    private ArrayList<RelativeLayout> warmupButtons = new ArrayList<RelativeLayout>();
     private LineChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.layout);
+        initToolbar();
+        initViewPager();
+    }
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
+    private void initToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Set an OnMenuItemClickListener to handle menu item clicks
 
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                // Handle the menu item
 
-        toolbarTitle = "Fitness app";
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerList = (ListView) findViewById(R.id.left_drawer);
-
-        drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, drawerMenuItems));
-        drawerList.setOnItemClickListener(new DrawerItemClickListener());
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, myToolbar, R.string.drawer_open, R.string.drawer_close) {
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                getSupportActionBar().setTitle(toolbarTitle);
+                return true;
             }
+        });
+        // Inflate a menu to be displayed in the toolbar
 
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                getSupportActionBar().setTitle(toolbarTitle);
-            }
-        };
-
-        drawerLayout.addDrawerListener(drawerToggle);
+        toolbar.inflateMenu(R.menu.workout_menu);
+        setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        configureGraph();
-        initializeWarmupViews(5);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mActionBarDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
     }
+
+    private void initViewPager(){
+        mTabs = (android.support.design.widget.TabLayout) findViewById(R.id.tabs);
+        mTabs.addTab(mTabs.newTab().setText("Tab 1"));
+        mTabs.addTab(mTabs.newTab().setText("Tab 2"));
+        mTabs.addTab(mTabs.newTab().setText("Tab 3"));
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new SamplePagerAdapter());
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabs));
+    }
+
 
     public float dpToPixels(float dp) {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
@@ -93,7 +122,41 @@ public class MainActivity extends AppCompatActivity {
         return dp;
     }
 
-    private ArrayList<RelativeLayout> warmupButtons = new ArrayList<RelativeLayout>();
+    private class SamplePagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return o == view;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Item " + (position + 1);
+        }
+
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View view = getLayoutInflater().inflate(R.layout.pager_item,
+                    container, false);
+            container.addView(view);
+            TextView title = (TextView) view.findViewById(R.id.item_title);
+            title.setText(String.valueOf(position + 1));
+            return view;
+        }
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+    }
+
+
 
     public void initializeWarmupViews(int warmups) {
         LayoutParams lp = new LayoutParams((int) dpToPixels(40), (int) dpToPixels(40));
@@ -222,63 +285,42 @@ public class MainActivity extends AppCompatActivity {
         mChart.setData(data);
     }
 
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.workout_menu, menu);
+
+        //MenuItem menuSearchItem = menu.findItem(R.id.my_search);
+        // Get the SearchView and set the searchable configuration
+
+        //SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//        SearchView searchView = (SearchView) menuSearchItem.getActionView();
+
+        //SearchView searchView = (SearchView) menuSearchItem.getActionView();
+        // Assumes current activity is the searchable activity
+
+        //searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        //searchView.setIconifiedByDefault(true); // Do not iconify the widget; expand it by default
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        } else if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                // About option clicked.
 
-        return super.onOptionsItemSelected(item);
-    }
+                return true;
+            case R.id.action_exit:
+                // Exit option clicked.
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        drawerToggle.syncState();
-    }
+                return true;
+            case R.id.action_settings:
+                // Settings option clicked.
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    /**
-     * Swaps fragments in the main content view
-     */
-    private void selectItem(int position) {
-        Toast.makeText(this, R.string.app_name, Toast.LENGTH_SHORT).show();
-
-        // Highlight the selected item, update the title, and close the drawer
-        drawerList.setItemChecked(position, true);
-        setTitle(drawerMenuItems[position]);
-        drawerLayout.closeDrawer(drawerList);
-    }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        toolbarTitle = title;
-        getSupportActionBar().setTitle(toolbarTitle);
-    }
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView parent, View view, int position, long id) {
-            selectItem(position);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
